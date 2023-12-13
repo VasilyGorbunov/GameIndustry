88fc982e0674a863281c19e501d4f1a19ee22196
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-12-13 14:50:00 +0300

    6 - done

---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GameIndustry.Pages
+{
+    public partial class Counter
+    {
+        [Inject] SingletonService singleton { get; set; }
+        [Inject] TransientService transient { get; set; }
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+
+            singleton.Value = currentCount;
+            transient.Value = currentCount;
+        }
+    }
+}
